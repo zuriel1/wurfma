ba0b25ab3b17f0599e972344d087cb4657c2cf22
Zuriel Williams <zuriel.wz@gmail.com>
2015-07-24 23:43:13 +0000

    Setting homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Wurfma::Application.routes.draw do
+  root 'recipes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
