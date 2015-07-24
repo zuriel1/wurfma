45e5e76775d6593607ca8f82741bbe6986887c03
Zuriel Williams <zuriel.wz@gmail.com>
2015-07-24 20:08:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wurfma::Application.config.secret_key_base = 'b208486c91e1698861cfe6c6a94fa06e38bf53a82651a92aa73a8cb21f1790bda0548b1ed393855e8585a487fc9713ca2cf091b8b187ca15bca0241b0b44aa09'
