49492d916135eae7f37c95aeac9fa60ef0b47c78
Zuriel Williams <zuriel.wz@gmail.com>
2015-07-25 12:37:48 +0000

    listing on index

--- db/migrate/20150724234954_create_recipes.rb
+++ db/migrate/20150724234954_create_recipes.rb
@@ -1,10 +1,11 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-      t.string :dish
-      t.text :ingredient
-      t.text :steps	
       
+      t.string :dish
+      t.string :ingredient
+      t.string :steps	
+
       t.timestamps 
     end
   end
